d786702627daf4117e54056ce5c86187fc804f74
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-04-17 11:14:20 +0100

    Point 2.1.3 Development
    Added atributes to vehicle and update function.

--- proj/MyVehicle.js
+++ proj/MyVehicle.js
@@ -1,6 +1,11 @@
 class MyVehicle extends CGFobject {
 	constructor(scene) {
-		super(scene);
+        super(scene);
+        
+        this.position = [0, 0, 0];
+        this.velocity = 0;
+        this.angle = 0;
+        
         this.initBuffers();
         this.initMaterials();
         this.initTextCoords();
@@ -30,8 +35,18 @@ class MyVehicle extends CGFobject {
     display() {
         this.scene.pushMatrix();
         this.material.apply();
+
+        this.scene.translate(this.position[0], this.position[1], this.position[2]);
+        this.scene.rotate(this.angle, 0, 1, 0);
+
         this.scene.rotate(Math.PI/2, 1, 0, 0);
         this.pyramid.display();
         this.scene.popMatrix();
+    }
+
+    update() {
+        this.position[0] = this.position[0] + this.velocity * (-Math.sin(this.angle));  // X
+        this.position[1] = this.position[1];                                            // Y
+        this.position[2] = this.position[2] + this.velocity * Math.cos(this.angle);     // Z
     }
 }
