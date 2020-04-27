80ac01e6cd6f4605e4d39bf849c94fa0c6aaed6e
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-04-27 19:07:25 +0100

    Point 2.1.2

--- proj/MyScene.js
+++ proj/MyScene.js
@@ -98,7 +98,28 @@ class MyScene extends CGFscene {
 
     // called periodically (as per setUpdatePeriod() in init())
     update(t){
+        this.checkKeys();
         //To be done...
+    }
+
+    checkKeys() {
+        var text = "Keys Pressed: ";
+        var keysPressed = false;
+
+        // Check for key codes e.g. in https://keycode.info/
+        if (this.gui.isKeyPressed("KeyW")) {
+            text += "W ";
+            keysPressed = true;
+        }
+
+        if (this.gui.isKeyPressed("KeyS")) {
+            text += "S ";
+            keysPressed = true;
+        }
+
+        if (keysPressed) {
+            console.log(text);
+        }
     }
 
     updateAppliedTexture() {
