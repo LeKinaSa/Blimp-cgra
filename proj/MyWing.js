466395421c42d70d406de3b8fc55157038c27750
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-04-27 18:27:58 +0100

    Completed Wing

--- proj/MyWing.js
+++ proj/MyWing.js
@@ -9,6 +9,8 @@ class MyWing extends CGFobject {
 	initBuffers() {
         this.triangle1 = new MyTriangle(this.scene);
         this.triangle2 = new MyTriangle(this.scene);
+        this.square1 = new MyQuad(this.scene);
+        this.square2 = new MyQuad(this.scene);
     }
     
     initTextCoords() {
@@ -22,16 +24,27 @@ class MyWing extends CGFobject {
 
     display() {
         this.scene.pushMatrix();
-        this.scene.translate(1, 1, 0);
-        this.scene.scale(1, -1, 1);
+        this.scene.translate(0.5, 0.5, 0);
+        this.scene.scale(0.5, -0.5, 0.5);
         this.triangle1.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate(1, 1, 0);
+        this.scene.translate(0.5, 0.5, 0);
         this.scene.rotate(-Math.PI/2, 0, 0, 1);
-        this.scene.scale(1, 1, -1);
+        this.scene.scale(0.5, 0.5, -0.5);
         this.triangle2.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0.5, 0);
+        this.square1.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0.5, 0);
+        this.scene.scale(-1, 1, 1);
+        this.square2.display();
         this.scene.popMatrix();
     }
 }
