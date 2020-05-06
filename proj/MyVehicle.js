39506baf9d91cc8173243bbc4398c3db029e3ea1
LeKina <LeKinaSa>
2020-05-06 18:43:06 +0100

    Completed Point 3.1

--- proj/MyVehicle.js
+++ proj/MyVehicle.js
@@ -12,12 +12,18 @@ class MyVehicle extends CGFobject {
     }
     
 	initBuffers() {
-        this.wingTop   = new MyWing(this.scene);
-        this.wingBot   = new MyWing(this.scene);
-        this.wingLeft  = new MyWing(this.scene);
-        this.wingRight = new MyWing(this.scene);
-        this.body      = new MyBody(this.scene);
-        this.cabin     = new MyBody(this.scene);
+        this.wingTop     = new MyWing  (this.scene);
+        this.wingBot     = new MyWing  (this.scene);
+        this.wingLeft    = new MyWing  (this.scene);
+        this.wingRight   = new MyWing  (this.scene);
+        this.body        = new MySphere(this.scene, 16, 16);
+        this.cabin       = new MyBody  (this.scene);
+        this.motorLeft   = new MyBody  (this.scene);
+        this.motorRight  = new MyBody  (this.scene);
+        this.heliceLeft  = new MyHelice(this.scene);
+        this.heliceRight = new MyHelice(this.scene);
+        this.centerRight = new MySphere(this.scene, 16, 16);
+        this.centerLeft  = new MySphere(this.scene, 16, 16);
     }
     
     initMaterials() {
@@ -34,6 +40,7 @@ class MyVehicle extends CGFobject {
     }
 
     display() {
+        //this.scene.scale(10, 10, 10);
         this.scene.pushMatrix(); // 1
         this.material.apply();
         
@@ -43,49 +50,97 @@ class MyVehicle extends CGFobject {
         
         this.scene.pushMatrix(); // 2
 
+        // Wing Top
         this.scene.pushMatrix();
-        this.scene.translate(0, 0.6, -1);
+        this.scene.translate(0, 0.5, -0.7);
         this.scene.rotate(-Math.PI/2, 0, 1, 0);
-        this.scene.scale(0.5, 0.5, 1);
+        this.scene.scale(0.4, 0.4, 1);
         this.wingTop.display();
         this.scene.popMatrix();
 
+        // Wing Bot
         this.scene.pushMatrix();
-        this.scene.translate(0, -0.6, -1);
+        this.scene.translate(0, -0.5, -0.7);
         this.scene.rotate(-Math.PI/2, 0, 1, 0);
         this.scene.scale(1, -1, 1);
-        this.scene.scale(0.5, 0.5, 1);
+        this.scene.scale(0.4, 0.4, 1);
         this.wingBot.display();
         this.scene.popMatrix();
 
+        this.scene.popMatrix(); // 2
+
+        // Wing Left
         this.scene.pushMatrix();
-        this.scene.translate(-0.6, 0, -1);
+        this.scene.translate(-0.5, 0, -0.7);
         this.scene.rotate(-Math.PI/2, 0, 1, 0);
         this.scene.rotate(-Math.PI/2, 1, 0, 0);
         this.scene.scale(1, -1, 1);
-        this.scene.scale(0.5, 0.5, 1);
+        this.scene.scale(0.4, 0.4, 1);
         this.wingLeft.display();
         this.scene.popMatrix();
 
+        // Wing Right
         this.scene.pushMatrix();
-        this.scene.translate(0.6, 0, -1);
+        this.scene.translate(0.5, 0, -0.7);
         this.scene.rotate(-Math.PI/2, 0, 1, 0);
         this.scene.rotate(-Math.PI/2, 1, 0, 0);
-        this.scene.scale(0.5, 0.5, 1);
+        this.scene.scale(0.4, 0.4, 1);
         this.wingRight.display();
         this.scene.popMatrix();
-
-        this.scene.popMatrix(); // 2
         
+        // Body
         this.scene.pushMatrix();
         this.scene.scale(0.5, 0.5, 1);
         this.body.display();
         this.scene.popMatrix();
 
+        // Cabin
         this.scene.pushMatrix();
-        this.scene.translate(0, -0.6, 0.2);
-        this.scene.scale(0.2, 0.2, 0.4);
+        this.scene.translate(0, -0.5, 0.2);
+        this.scene.scale(0.2, 0.2, 0.3);
         this.cabin.display();
+        this.scene.popMatrix();
+
+        // Motor Right
+        this.scene.pushMatrix();
+        this.scene.translate(0.15, -0.55, -0.15);
+        this.scene.scale(0.07, 0.07, 0.08);
+        this.motorRight.display();
+        this.scene.popMatrix();
+
+        // Motor Left
+        this.scene.pushMatrix();
+        this.scene.translate(-0.15, -0.55, -0.15);
+        this.scene.scale(0.07, 0.07, 0.08);
+        this.motorLeft.display();
+        this.scene.popMatrix();
+
+        // Helice Right
+        this.scene.pushMatrix();
+        this.scene.translate(0.15, -0.55, -0.28);
+        this.scene.scale(0.15, 0.125, 0.125);
+        this.heliceRight.display();
+        this.scene.popMatrix();
+
+        // Helice Left
+        this.scene.pushMatrix();
+        this.scene.translate(-0.15, -0.55, -0.28);
+        this.scene.scale(0.15, 0.125, 0.125);
+        this.heliceLeft.display();
+        this.scene.popMatrix();
+
+        // Helice Center Right
+        this.scene.pushMatrix();
+        this.scene.translate(0.15, -0.55, -0.28);
+        this.scene.scale(0.02, 0.02, 0.02);
+        this.centerRight.display();
+        this.scene.popMatrix();
+
+        // Helice Center Left
+        this.scene.pushMatrix();
+        this.scene.translate(-0.15, -0.55, -0.28);
+        this.scene.scale(0.02, 0.02, 0.02);
+        this.centerLeft.display();
         this.scene.popMatrix();
 
         this.scene.popMatrix(); // 1
